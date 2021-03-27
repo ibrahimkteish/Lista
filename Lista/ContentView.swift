b76e8a562dde84a6c561b891ceb592dfb14152fc
Ibrahim Koteish <ikteish@yahoo.com>
2021-03-27 14:29:53 +0100

    initial commit

---
+++ Lista/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Lista
+//
+//  Created by Ibrahim Koteish on 27/03/2021.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lista/ListaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListaApp.swift
+//  Lista
+//
+//  Created by Ibrahim Koteish on 27/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct ListaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
